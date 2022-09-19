42e1e5343139559b776019c4388e675a44eb6281
CheeTaHOO7 <109506738+CheeTaHOO7@users.noreply.github.com>
2022-09-19 14:14:05 +0530

    Create sum-digits.c

---
+++ sum-digits.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num, r, sum = 0;
+    printf("Enter any number:\n");
+    scanf("%d", &num);
+    while(num != 0){
+        r = num % 10;
+        num = num / 10;
+        sum = sum + r;
+    }
+    printf("Sum of the digits is: %d", sum);
+    return 0;
+}
