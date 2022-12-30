9b7922ce4e992bcc7444c8df6d5503715bc20d8b
Akhil Singh Chauhan <109506738+akhil-OO7@users.noreply.github.com>
2022-12-30 12:07:45 +0530

    Create prac5-6.c

---
+++ Practical/practical-5/prac5-6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int min(int, int);
+int max(int, int);
+int main() {
+    int arr[20], size, copy[20], i;
+    printf("Enter size of the array: ");
+    scanf("%d", &size);
+    printf("Enter %d elements of an array: ", size);
+    for(i = 0; i < size; i++)
+        scanf("%d", &arr[i]);
+    for(int j = size - 1, i = 0; j >= 0; j--, i++)
+        copy[i] = arr[j];
+    printf("Reversed array is: ");
+    for(i = 0; i < size; i++)
+        printf("%d ", copy[i]);
+        
+    return 0;
+}
+        
