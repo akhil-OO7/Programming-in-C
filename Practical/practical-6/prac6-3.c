b9c7095db58f551a8d3b0efbe423029a1333f471
Akhil Singh Chauhan <109506738+akhil-OO7@users.noreply.github.com>
2023-01-12 14:38:58 +0530

    Create prac6-3.c

---
+++ Practical/practical-6/prac6-3.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+int main() {
+    int a[10][10], b[10][10], n, sum[10][10], mul[10][10];
+    printf("Enter the size of 2 matrices (n x n): ");
+    scanf("%d", &n);
+    
+    printf("Enter elements of A matrix (%d x %d): ", n, n);
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j ++)
+            scanf("%d", &a[i][j]);
+    printf("Enter elements of B matrix (%d x %d): ", n, n);
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j ++)
+            scanf("%d", &b[i][j]);
+    printf("A matrix: \n");
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("B Matrix: \n");
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            printf("%d ", b[i][j]);
+        }
+        printf("\n");
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    printf("Sum of A + B =  \n");
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            mul[i][j] = 0;
+            for(int k = 0; k < n; k++){
+                mul[i][j] += a[i][k] * b[k][j]; 
+            }
+        }
+    }
+    printf("Product of A * B = \n");
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            printf("%d ", mul[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
